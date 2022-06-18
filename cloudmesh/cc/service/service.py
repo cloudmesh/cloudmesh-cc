bafd12aca241329774d8b5ba414af6b3720de030
Diyaolu04 <104225033+Diyaolu04@users.noreply.github.com>
2022-06-18 15:19:02 -0400

    Update

--- cloudmesh/cc/service/service.py
+++ cloudmesh/cc/service/service.py
@@ -2,24 +2,33 @@
 from fastapi import FastAPI
 import logging
 
+#app = FastAPI()
+
+#items = [{"name": "Foo"}, {"name": "Bar"}, {"name": "Baz"}]
+
+#@app.get("/")
+#async def read_main():
+  #  return {"msg": "Hello World"}
+
+#@app.get("/items/")
+#async def read_items():
+    #return {"msg": "Hello World"}
+
+#@app.get("/search/")
+#async def search_item(name:str):
+   # result = None
+  #  for item in items:
+      #  if item['name'] == name:
+            #result = name
+   # return result
+
+
+import uvicorn
+from fastapi import FastAPI
+
 app = FastAPI()
 
-items = [{"name": "Foo"}, {"name": "Bar"}, {"name": "Baz"}]
 
 @app.get("/")
-async def read_main():
-    return {"msg": "Hello World"}
-
-@app.get("/items/")
-async def read_items():
-    return {"msg": "Hello World"}
-
-@app.get("/search/")
-async def search_item(name:str):
-    result = None
-    for item in items:
-        if item['name'] == name:
-            result = name
-    return result
-
-
+async def read_root():
+    return {"Hello": "World"}
