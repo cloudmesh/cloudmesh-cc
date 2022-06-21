55e2040d492608cedf896a95f16eed5ea41ece86
Alison Lu <alibob12401@hotmail.com>
2022-06-21 09:03:40 -0400

    setup.py change

--- setup.py
+++ setup.py
@@ -32,6 +32,7 @@ cloudmesh-cmd5
 cloudmesh-sys
 cloudmesh-inventory
 cloudmesh-configuration
+fastapi[all]
 """.split("\n")
 
 # dependency_links = ['http://github.com/nicolaiarocci/eve.git@develop']
