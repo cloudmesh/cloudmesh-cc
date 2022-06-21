d5b9cc193fd15040dc7017d7726c39ba50478e00
Alex Beck <abeckct@gmail.com>
2022-06-21 16:33:07 -0400

    Modified test file

--- tests/test_cc.py
+++ tests/test_cc.py
@@ -29,22 +29,12 @@ class TestConfig:
         QUEUES = 'testqueue'
         DATABASE= 'yamldb'
         Benchmark.Start()
-<<<<<<< HEAD
-        result = Shell.run(f"cms cc create --queues={QUEUES} "
-                               f"--database={DATABASE}")
-        Benchmark.Stop()
-        VERBOSE(result)
-        assert '' in result
-        result = Shell.run(f'cms cc list --queue={QUEUES}')
-        print(result)
-=======
         result = Shell.execute("cms cc create --queue=a,b,c --database=\'yamldb\'", shell=True)
         Benchmark.Stop()
         VERBOSE(result)
         print(result)
         assert "a" in result
         assert "b" in result
->>>>>>> 851bfd967cf9c5ff2c82407b906e5004f3ed704a
 
     def test_benchmark(self):
         HEADING()
