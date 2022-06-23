31b08122b047c0a6106aa81249c2f964affefc6d
Alex Beck <abeckct@gmail.com>
2022-06-23 16:58:57 -0400

    Modified service.py

--- cloudmesh/cc/service/service.py
+++ cloudmesh/cc/service/service.py
@@ -1,26 +1,23 @@
 from fastapi import FastAPI
 import logging
+from cloudmesh.cc.queue import Queues
+from cloudmesh.common.Printer import Printer
+from fastapi.responses import HTMLResponse
 
-# app = FastAPI()
+def test_run():
+    kind = 'yamldb'
+    q = Queues(filename='~/.cloudmesh/queue/queuetest1', database=kind)
+    q.create(name='local')
+    q.create(name='rivanna')
+    q.add(name='local', job='job-1', command='pwd')
+    q.add(name='local', job='job-2', command='ls')
+    q.add(name='local', job='job-3', command='hostname')
+    q.add(name='rivanna', job='job-1', command='pwd')
+    q.add(name='rivanna', job='job-2', command='ls')
+    q.add(name='rivanna', job='job-3', command='hostname')
+    return q
 
-# items = [{"name": "Foo"}, {"name": "Bar"}, {"name": "Baz"}]
-
-# @app.get("/")
-# async def read_main():
-#  return {"msg": "Hello World"}
-
-# @app.get("/items/")
-# async def read_items():
-# return {"msg": "Hello World"}
-
-# @app.get("/search/")
-# async def search_item(name:str):
-# result = None
-#  for item in items:
-#  if item['name'] == name:
-# result = name
-# return result
-
+q = test_run()
 
 import uvicorn
 from fastapi import FastAPI
@@ -37,6 +34,40 @@ async def read_main():
     return {"msg": "Hello World"}
 
 
-@app.get("/items/")
+@app.get("/items/", response_class=HTMLResponse)
 async def read_items():
-    return {"msg": "Hello World"}
+    global q
+    n = []
+    for queue in q.queues:
+        for job in q.queues[queue]:
+            n.append(q.queues[queue][job])
+
+    result = Printer.write(n, output='html')
+    result = result.replace('\n', '')
+    page = f"""
+        <html>
+            <head>
+                <title>Some HTML in here</title>
+            </head>
+            <body>
+                {result}
+            </body>
+        </html>
+        """
+    return page
+
+@app.get("/item/{queue}/{job}", response_class=HTMLResponse)
+async def read_job(queue:str, job:str):
+    global q
+    result = Printer.attribute(q.queues[queue][job], output='html')
+    page = f"""
+    <html>
+        <head>
+            <title>Some HTML in here</title>
+        </head>
+        <body>
+            {result}
+        </body>
+    </html>
+    """
+    return page
