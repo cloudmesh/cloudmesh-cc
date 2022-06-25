75031da46b5f452686f7dd73cc31b2c4c3766a4a
J.P <stapmoshun@gmail.com>
2022-06-25 01:34:55 -0400

    slurm docker

--- docker/slurm/manager/docker-entrypoint.sh
+++ docker/slurm/manager/docker-entrypoint.sh
@@ -1,8 +1,9 @@
-#!/bin/sh
+#!/bin/bash
 
 sudo sed -i "s/REPLACE_IT/CPUs=$(nproc)/g" /etc/slurm/slurm.conf
 
 sudo service munge start
 sudo service slurmctld start
+sudo cp /etc/munge/munge.key /home/admin/
 
 tail -f /dev/null
