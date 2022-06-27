9c5a291893a30e26dd87912c2fb53b1383143299
Gregor von Laszewski <laszewski@gmail.com>
2022-06-27 19:56:20 -0400

    add run.sh

---
+++ tests/run.sh
@@ -0,0 +1,9 @@
+#!/bin/bash -x
+date
+echo "# cloudmesh status=running progress=0"
+ls
+echo "# cloudmesh status=running progress=10"
+pwd
+echo "# cloudmesh status=running progress=20"
+hostname
+echo "# cloudmesh status=done progress=100"
