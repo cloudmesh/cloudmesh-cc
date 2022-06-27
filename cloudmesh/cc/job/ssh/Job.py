f89d719770d9da0bb77770fa22338faf4de8c604
Diyaolu04 <104225033+Diyaolu04@users.noreply.github.com>
2022-06-27 13:28:39 -0400

    log and progress

--- cloudmesh/cc/job/ssh/Job.py
+++ cloudmesh/cc/job/ssh/Job.py
@@ -1,12 +1,11 @@
-from cloudmesh.cc.job.AbstractJob import AbstractJob
-# from cloudmesh.common FIND SOMETHING THAT READS TEXT FILES
 
+from cloudmesh.cc.job.AbstractJob import AbstractJob
+from cloudmesh.common.util import readfile
 class Job(AbstractJob):
 
     def get_log(self):
-        return
-
+        return readfile('run.log', 'r')
 
     def get_progress(self):
-        prog = TextFinder.find("progress=")
+        prog = TextFinder.find("progress=", self.get_log())
         return prog
