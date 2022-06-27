551163539e03ef7b588597c3d3c21a5537c5d3e5
Jackson Miskill <jcmiskill@gmail.com>
2022-06-27 13:18:23 -0400

    committing changes so I can pull

--- cloudmesh/cc/job/ssh/Job.py
+++ cloudmesh/cc/job/ssh/Job.py
@@ -1,6 +1,47 @@
 from cloudmesh.cc.job.AbstractJob import AbstractJob
+from cloudmesh.common.util import readfile
+from cloudmesh.common.util import writefile
 
 class Job(AbstractJob):
 
-    def __int__(self):
-        #
+    def __init__(self):
+        s
+
+    def probe(self):
+        pass
+
+    def run(self):
+        pass
+
+    def get_status(self):
+        pass
+
+    def get_error(self):
+
+        pass
+
+    def get_log(self):
+        pass
+
+    def get_progress(self):
+        pass
+
+    def sync(self):
+
+        pass
+
+    @property
+    def status(self):
+
+        return self.get_status()
+
+    @property
+    def get(self, attribute):
+
+        pass
+
+    def progress(self):
+        return self.get("progress")
+
+    def watch(self, period=10):
+
