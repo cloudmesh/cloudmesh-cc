789493da52c1adc0a01a41f876892858ba398cd7
al07734 <alibob12401@hotmail.com>
2022-06-27 12:14:04 -0400

    Job run

--- cloudmesh/cc/job/ssh/Job.py
+++ cloudmesh/cc/job/ssh/Job.py
@@ -1,12 +1,22 @@
 from cloudmesh.cc.job.AbstractJob import AbstractJob
 # from cloudmesh.common FIND SOMETHING THAT READS TEXT FILES
+from cloudmesh.common.Shell import Shell
+
+
 
 class Job(AbstractJob):
 
+    def run(self):
+        Shell.sh("./run.sh atl9rn")
+
     def get_log(self):
-        return
+        return True
 
 
     def get_progress(self):
         prog = TextFinder.find("progress=")
         return prog
+
+
+j = Job()
+print(j.run())
