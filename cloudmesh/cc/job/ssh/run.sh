3f2dddb312804d56ba7a4e6fbd4944859b1a4c4b
Gregor von Laszewski <laszewski@gmail.com>
2022-06-28 09:16:50 -0400

    Alex local host for mac

--- cloudmesh/cc/job/ssh/run.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash -x
-date
-echo "# cloudmesh status=running progress=0"
-ls
-echo "# cloudmesh status=running progress=10"
-pwd
-echo "# cloudmesh status=running progress=20"
-hostname
-echo "# cloudmesh status=done progress=100"
--- cloudmesh/cc/job/ssh/test.sh
+++ cloudmesh/cc/job/ssh/test.sh
@@ -3,6 +3,9 @@ username="$1"
 
 scp run.sh "$username"@rivanna.hpc.virginia.edu:.
 ssh "$username"@rivanna.hpc.virginia.edu cat run.sh
+
+# cat run.sh
+
 ssh "$username"@rivanna.hpc.virginia.edu "nohup ./run.sh > run.log 2>run.error; echo $pid"
 #ssh "$username"@rivanna.hpc.virginia.edu "nohup ./run.sh > run.error; echo $pid"
 scp "$username"@rivanna.hpc.virginia.edu:run.log run.log
