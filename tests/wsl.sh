615afee772e6345b5c05060ed08c3669bb394c50
Alex Beck <abeckct@gmail.com>
2022-07-01 14:28:39 -0400

    Updated wsl.sh

--- tests/wsl.sh
+++ tests/wsl.sh
@@ -1,26 +1,25 @@
 #!/bin/bash -x
-USERNAME="$1"
 PWD=`pwd`
 echo $PWD
 H="/mnt$PWD"
 
-rm -f run.log
-rm -f run.error
+rm -f run-wsl.log
+rm -f run-wsl.error
 
-wsl sh -c ". ~/.profile && cd /mnt/c/Users/$USERNAME && rm -f run.sh"
-wsl sh -c ". ~/.profile && cd /mnt/c/Users/$USERNAME && rm -f run.log"
-wsl sh -c ". ~/.profile && cd /mnt/c/Users/$USERNAME && rm -f run.err"
+wsl sh -c ". ~/.profile && cd /mnt/c/Users/$USERNAME && rm -f run-wsl.sh"
+wsl sh -c ". ~/.profile && cd /mnt/c/Users/$USERNAME && rm -f run-wsl.log"
+wsl sh -c ". ~/.profile && cd /mnt/c/Users/$USERNAME && rm -f run-wsl.err"
 
-wsl sh -c ". ~/.profile && cp run.sh /mnt/c/Users/$USERNAME/run.sh"
-wsl sh -c ". ~/.profile && cd /mnt/c/Users/$USERNAME && cat run.sh"
+wsl sh -c ". ~/.profile && cp run-wsl.sh /mnt/c/Users/$USERNAME/run-wsl.sh"
+wsl sh -c ". ~/.profile && cd /mnt/c/Users/$USERNAME && cat run-wsl.sh"
 
-# cat run.sh
+# cat run-wsl.sh
 
-wsl nohup sh -c ". ~/.profile && cd /mnt/c/Users/$USERNAME && ./run.sh &"
+wsl nohup sh -c ". ~/.profile && cd /mnt/c/Users/$USERNAME && ./run-wsl.sh &"
 sleep 2
-wsl sh -c ". ~/.profile && cp /mnt/c/Users/$USERNAME/run.log $H/run.log"
-wsl sh -c ". ~/.profile && cp /mnt/c/Users/$USERNAME/run.error $H/run.error"
+wsl sh -c ". ~/.profile && cp /mnt/c/Users/$USERNAME/run-wsl.log $H/run-wsl.log"
+wsl sh -c ". ~/.profile && cp /mnt/c/Users/$USERNAME/run-wsl.error $H/run-wsl.error"
 
-cat run.log
+cat run-wsl.log
 
 
