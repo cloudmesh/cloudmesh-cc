779280be7e0aa488dad25cca7c7fecdc9138949a
Alex Beck <abeckct@gmail.com>
2022-07-01 10:56:27 -0400

    Finalized wsl command

--- tests/alex.py
+++ tests/alex.py
@@ -7,10 +7,14 @@ try:
     # print(reset)
     # change = os.system('cd tests')
     # print(change)
+    # sed -i -e 's/\r$//'
     username = os.environ["USERNAME"]
+    print(username)
     directory = f'/mnt/c/Users/{username}/cm/cloudmesh-cc/tests/'
-    Shell.run(f'start /max wsl sh -c ". ~/.profile && cd {directory} && '
-              f'./run.sh"')
+    # Shell.run(f'start /max wsl sh -c ". ~/.profile && cd {directory} && '
+    #           f'./run.sh"')
+    Shell.run(f'wsl nohup sh -c ". ~/.profile && cd {directory} && '
+              f'./run.sh &"')
     # reset = f'wsl cd //mnt//c//'
     # pwd = f'wsl "pwd"'
     # r = Shell.run(reset)
