1f452329c3c6f58597ebe84ec933308628934155
Alex Beck <abeckct@gmail.com>
2022-07-01 15:40:47 -0400

    Updated Job.py

--- tests/wsl.sh
+++ tests/wsl.sh
@@ -3,20 +3,24 @@ PWD=`pwd`
 echo $PWD
 H="/mnt$PWD"
 
+# remove local files
 rm -f run-wsl.log
 rm -f run-wsl.error
 
+# cd into wsl and remove wsl files
 wsl sh -c ". ~/.profile && cd /mnt/c/Users/$USERNAME && rm -f run-wsl.sh"
 wsl sh -c ". ~/.profile && cd /mnt/c/Users/$USERNAME && rm -f run-wsl.log"
 wsl sh -c ". ~/.profile && cd /mnt/c/Users/$USERNAME && rm -f run-wsl.err"
 
+# copy from local into wsl
 wsl sh -c ". ~/.profile && cp run-wsl.sh /mnt/c/Users/$USERNAME/run-wsl.sh"
 wsl sh -c ". ~/.profile && cd /mnt/c/Users/$USERNAME && cat run-wsl.sh"
 
-# cat run-wsl.sh
-
+# run
 wsl nohup sh -c ". ~/.profile && cd /mnt/c/Users/$USERNAME && ./run-wsl.sh &"
 sleep 2
+
+# sync
 wsl sh -c ". ~/.profile && cp /mnt/c/Users/$USERNAME/run-wsl.log $H/run-wsl.log"
 wsl sh -c ". ~/.profile && cp /mnt/c/Users/$USERNAME/run-wsl.err $H/run-wsl.err"
 
