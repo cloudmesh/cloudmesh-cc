a1e4ab37a93ad7444548671baee3c9b42b5ff041
Jackson Miskill <jcmiskill@gmail.com>
2022-07-01 13:35:43 -0400

    updated the test.sh file

---
+++ tests/test.sh
@@ -0,0 +1,22 @@
+#!/bin/bash -x
+username="$1"
+
+rm -f run.log
+rm -f run.error
+
+
+ssh "$username"@rivanna.hpc.virginia.edu rm -f run.sh
+ssh "$username"@rivanna.hpc.virginia.edu rm -f run.log
+ssh "$username"@rivanna.hpc.virginia.edu rm -f run.error
+
+scp run.sh "$username"@rivanna.hpc.virginia.edu:.
+ssh "$username"@rivanna.hpc.virginia.edu cat run.sh
+
+# cat run.sh
+
+ssh "$username@rivanna.hpc.virginia.edu \"nohup ./run.sh > run.log 2>run.error\" &"
+sleep 2
+scp "$username"@rivanna.hpc.virginia.edu:run.log run.log
+scp "$username"@rivanna.hpc.virginia.edu:run.error run.error
+
+cat run.log
