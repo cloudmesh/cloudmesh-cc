56a9d9d32e5affafb9d0ebd92f959447996498a3
J.P <stapmoshun@gmail.com>
2022-07-05 08:48:32 -0400

    fix wsl.sh

--- examples/wsl.sh
+++ examples/wsl.sh
@@ -1,30 +1,31 @@
 #!/bin/sh -x
+# run by standing in cloudmesh-cc/tests and execute ../examples/wsl.sh
 PWD=`pwd`
 echo $PWD
 H="/mnt$PWD"
 
 # remove local files
-rm -f run-wsl.log run-wsl.error
+rm -f run.log run.error
 
 # cd into wsl and remove wsl files
-wsl sh -c ". ~/.profile && cd /mnt/c/Users/$USERNAME && rm -f run-wsl.sh run-wsl.log run-wsl.err"
+wsl sh -c ". ~/.profile && cd /mnt/c/Users/$USERNAME && rm -f run.sh run.log run.err"
 
 # copy from local into wsl
-wsl sh -c ". ~/.profile && cp run-wsl.sh /mnt/c/Users/$USERNAME/run-wsl.sh"
-wsl sh -c ". ~/.profile && cd /mnt/c/Users/$USERNAME && cat run-wsl.sh"
+wsl sh -c ". ~/.profile && cp run.sh /mnt/c/Users/$USERNAME/run.sh"
+wsl sh -c ". ~/.profile && cd /mnt/c/Users/$USERNAME && cat run.sh"
 
 # run
-#wsl nohup sh -c ". ~/.profile && cd /mnt/c/Users/$USERNAME && ./run-wsl.sh > ./run-wsl.log 2>&1 &" >&/dev/null
-#wsl --cd /c/Users/$USERNAME nohup sh -c ". ~/.profile && ./run-wsl.sh > ./run-wsl.log 2>&1 &" >&/dev/null
-wsl --cd /c/Users/$USERNAME nohup sh -c "./run-wsl.sh > ./run-wsl.log 2>&1 &" >&/dev/null
+#wsl nohup sh -c ". ~/.profile && cd /mnt/c/Users/$USERNAME && ./run.sh > ./run.log 2>&1 &" >&/dev/null
+#wsl --cd /c/Users/$USERNAME nohup sh -c ". ~/.profile && ./run.sh > ./run.log 2>&1 &" >&/dev/null
+wsl --cd /c/Users/$USERNAME nohup sh -c "./run.sh > ./run.log 2>&1 &" >&/dev/null
 
 
-# wsl nohup sh -c ". ~/.profile && cd /mnt/c/Users/$USERNAME && ./run-wsl.sh &"
+# wsl nohup sh -c ". ~/.profile && cd /mnt/c/Users/$USERNAME && ./run.sh &"
 sleep 2
 
 # sync
-wsl sh -c ". ~/.profile && cp /mnt/c/Users/$USERNAME/run-wsl.log $H/run-wsl.log"
-#wsl sh -c ". ~/.profile && cp /mnt/c/Users/$USERNAME/run-wsl.err $H/run-wsl.err"
+wsl sh -c ". ~/.profile && cp /mnt/c/Users/$USERNAME/run.log $H/run.log"
+#wsl sh -c ". ~/.profile && cp /mnt/c/Users/$USERNAME/run.err $H/run.err"
 
-cat run-wsl.log
+cat run.log
 
