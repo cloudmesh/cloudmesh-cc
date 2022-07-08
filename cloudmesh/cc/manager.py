de069c9d5b0f7e0e24a258de3ab16e4ea9153b6a
Jackson Miskill <jcmiskill@gmail.com>
2022-07-08 15:33:06 -0400

    updated the manager.py cli workflow to have the proper :str syntax

--- cloudmesh/cc/manager.py
+++ cloudmesh/cc/manager.py
@@ -1,4 +1,7 @@
+from builtins import str
+
 import requests
+# when requests is imported, it has it's own "str", so we have to import the built-in
 from cloudmesh.cc.workflow import Workflow
 from cloudmesh.common.util import path_expand
 import os
@@ -9,7 +12,7 @@ class WorkflowCLIManager:
         self.name = name
 
 
-    def add_from_filename(self, filename=None):
+    def add_from_filename(self, filename:str=None):
         # cc workflow add [--name=NAME] [--job=JOB] ARGS...
         if self.name is None:
             self.name = os.path.basename(filename).replace(".yaml", "")
@@ -18,30 +21,30 @@ class WorkflowCLIManager:
         filename = path_expand(filename)
         w.load(filename)
 
-    def add_from_arguments(self, str, job=None:str, **argv):
+    def add_from_arguments(self, str, job:str=None, **argv):
         # cc workflow add [--name=NAME] [--job=JOB] ARGS...
         w = Workflow()
         w.load("tests/workflow.yaml")
 
         pass
 
-    def  delete (self, str, job=None:str):
+    def  delete (self, str, job:str=None):
         # cc workflow delete [--name=NAME] --job=JOB
         pass
 
-    def  list (self, job=None:str):
+    def  list (self, job:str=None):
         # cc workflow list [--name=NAME] [--job=JOB]
         pass
 
-    def  run (self, str, job=None:str, filename=None: str):
+    def  run (self, str, job:str=None, filename:str=None):
         # cc workflow run [--name=NAME] [--job=JOB] [--filename=FILENAME]
         pass
 
-    def dependencies(self,  dependencies=None: str):
+    def dependencies(self, dependencies:str=None):
         # cc workflow [--name=NAME] --dependencies=DEPENDENCIES
         pass
 
-    def  status (self, output=None: str):
+    def  status (self, output:str=None):
         # cc workflow status --name=NAME [--output=OUTPUT]
         pass
 
