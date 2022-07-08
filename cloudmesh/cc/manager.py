476888a92d79283e81fa9567bee49df1507df2fa
Jackson Miskill <jcmiskill@gmail.com>
2022-07-08 17:02:32 -0400

    updated manager.py with code inspection edits

--- cloudmesh/cc/manager.py
+++ cloudmesh/cc/manager.py
@@ -1,17 +1,22 @@
 from builtins import str
 
 import requests
-# when requests is imported, it has it's own "str", so we have to import the built-in
+# when requests is imported, it has its own "str", so we have to import the built-in
 from cloudmesh.cc.workflow import Workflow
 from cloudmesh.common.util import path_expand
 import os
 
+
 class WorkflowCLIManager:
 
-    def __init__(self, name:str=None):
+    def __init__(self, name: str = None):
+        self.filename = None
+        self.filename = None
+        self.filename = None
+        self.filename = None
         self.name = name
 
-    def add_from_filename(self, filename:str=None):
+    def add_from_filename(self, filename: str = None):
         # cc workflow add [--name=NAME] [--job=JOB] ARGS...
         if self.name is None:
             self.name = os.path.basename(filename).replace(".yaml", "")
@@ -20,64 +25,71 @@ class WorkflowCLIManager:
         filename = path_expand(filename)
         w.load(filename)
 
-    def add_from_arguments(self, job:str=None, filename:str=None, **argv):
+    def add_from_arguments(self, job: str = None, filename: str = None, **argv):
         # cc workflow add [--name=NAME] [--job=JOB] ARGS...
         data = argv
-        w = Workflow(filename=self.filename)
-        w.add_job(name=self.name, job=job, **data)
+        w = Workflow(filename=filename)
+        w.add_job(name=self.name, label=job, **data)
 
-    def  delete_job(self, job:str=None):
+    def delete_job(self, job: str = None):
         # cc workflow delete [--name=NAME] --job=JOB
         w = Workflow(filename=self.filename)
         w.remove_job(name=job)
 
-    def  delete_workflow(self, workflow:str=None):
+    def delete_workflow(self, filename: str = None):
         # cc workflow delete [--name=NAME] --job=JOB
         w = Workflow(filename=self.filename)
         w.remove_workflow()
 
-    def  list_job(self, job:str=None):
+    def list_job(self, job: str = None):
         # cc workflow list [--name=NAME] [--job=JOB]
         w = Workflow(filename=self.filename)
         j = w.job(name=job)
         print(j)
 
-    def  list_workflow(self, job:str=None, filename:str=None):
+    # noinspection PyMethodMayBeStatic
+    def list_workflow(self, filename: str = None):
         # cc workflow list [--name=NAME] [--filename=FILENAME]
         w = Workflow(filename=filename)
         nodes = w.jobs
         print(nodes)
 
-    def  run (self, str, job:str=None, filename:str=None):
+    # noinspection PyMethodMayBeStatic
+    def run(self, job: str = None, filename: str = None):
         # cc workflow run [--name=NAME] [--job=JOB] [--filename=FILENAME]
-        pass
+        w = Workflow(filename=filename)
+        w.run_parallel()
 
-    def dependencies(self, dependencies:str=None):
+    def dependencies(self, dependencies: str = None):
         # cc workflow [--name=NAME] --dependencies=DEPENDENCIES
         pass
 
-    def  status (self, output:str=None):
+    def status(self, output: str = None):
         # cc workflow status --name=NAME [--output=OUTPUT]
         pass
 
-    def  graph (self):
+    def graph(self):
         # cc workflow graph --name=NAME
         pass
 
 
+# noinspection PyAttributeOutsideInit
 class WorkflowServiceManager:
 
     def __init__(self, name=None, port=8000, host="127.0.0.1"):
+        self.name = None
         self.host = host
-        self.port=port
+        self.port = port
 
+    # noinspection PyMethodMayBeStatic
     def add_job(self, name=None, job=None, **argv):
         if name is None:
             name = "workflow"
         if job is None:
-            n=0 # read from config file
+            n = 0  # read from config file
             job = f"job-{n}"
-        r = requests.get('https://{self.host}:{self.port}/workflow?name={name}&job={job}')
+        r = requests.get(
+            'https://{self.host}:{self.port}/workflow?name={name}&job={job}')
         pass
 
     def add_from_filename(self, filename=None):
@@ -85,28 +97,23 @@ class WorkflowServiceManager:
         if self.name is None:
             self.name = os.path.basename(filename).replace(".yaml", "")
 
-        r = requests.get('https://{self.host}:{self.port}/workflow?name={name}&job={job}')
+        r = requests.get(
+            'https://{self.host}:{self.port}/workflow?name={name}&job={job}')
 
-
-    def delete(self, name:str=None, job:str=None):
+    def delete(self, name: str = None, job: str = None):
         pass
 
-
-    def list(self, name:str=None, job:str=None):
+    def list(self, name: str = None, job: str = None):
         pass
 
-
-    def run(self, name:str=None, job:str=None, filename:str=None):
+    def run(self, name: str = None, job: str = None, filename: str = None):
         pass
 
-
-    def dependencies(self, name: str, dependencies:str=None):
+    def dependencies(self, name: str, dependencies: str = None):
         pass
 
-
-    def status(self, name: str, output:str=None):
+    def status(self, name: str, output: str = None):
         pass
-
 
     def graph(self, name: str):
         pass
