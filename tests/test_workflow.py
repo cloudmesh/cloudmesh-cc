9a3dbcfa19ad8203f4b16935cf0686eafc1f28c5
Jackson Miskill <jcmiskill@gmail.com>
2022-07-20 16:26:14 -0400

    added in the tests for the remove job and remove workflow

--- tests/test_workflow.py
+++ tests/test_workflow.py
@@ -21,6 +21,7 @@ from cloudmesh.common.util import banner
 from cloudmesh.common.StopWatch import StopWatch
 from pathlib import Path
 from cloudmesh.common.util import banner
+from cloudmesh.common.util import path_expand
 
 banner(Path(__file__).name, c = "#", color="RED")
 
@@ -45,6 +46,77 @@ else:
     username = os.path.basename(os.environ["HOME"])
 
 w = None
+
+def create_dummy_workflow():
+    global dw
+    filename = path_expand('dummy-workflow.yaml')
+    dw = Workflow(name='dummy-workflow')
+    if os_is_windows():
+        localuser = os.environ["USERNAME"]
+    else:
+        try:
+            localuser = os.environ['USER']
+        except:
+            # docker image does not have user variable. so just do basename of home
+            localuser = os.system('basename $HOME')
+    login = {
+        "localhost": {"user": f"{localuser}", "host": "local"},
+        "rivanna": {"user": f"{username}", "host": "rivanna.hpc.virginia.edu"},
+        "pi": {"user": f"{localuser}", "host": "red"},
+    }
+
+    n = 0
+
+    user = login["localhost"]["user"]
+    host = login["localhost"]["host"]
+
+    if os_is_windows():
+        jobkind="wsl"
+    else:
+        jobkind="local"
+
+    dw.add_job(name="start", kind=jobkind, user=user, host=host)
+    dw.add_job(name="end", kind=jobkind, user=user, host=host)
+
+    # dw.add_job(name="start", kind="local", user=user, host=host)
+    # dw.add_job(name="end", kind="local", user=user, host=host)
+
+    # for host, kind in [("localhost", "local"),
+    #                    ("rivanna", "local")]:
+    for host, kind in [("localhost", jobkind),
+                       ("rivanna", jobkind)]:
+
+        # ("rivanna", "ssh")
+
+        print("HOST:", host)
+        user = login[host]["user"]
+        host = login[host]["host"]
+        # label = f'job-{host}-{n}'.replace('.hpc.virginia.edu', '')
+
+        label = "debug={cm.debug}\nhome={os.HOME}\n{name}\n{now.%m/%d/%Y, %H:%M:%S}\nprogress={progress}"
+
+        dw.add_job(name=f"job-{host}-{n}", label=label,  kind=kind, user=user, host=host)
+        n = n + 1
+        dw.add_job(name=f"job-{host}-{n}", label=label, kind=kind, user=user, host=host)
+        n = n + 1
+        dw.add_job(name=f"job-{host}-{n}", label=label, kind=kind, user=user, host=host)
+        n = n + 1
+
+        first = n - 3
+        second = n - 2
+        third = n - 1
+        dw.add_dependencies(f"job-{host}-{first},job-{host}-{second}")
+        dw.add_dependencies(f"job-{host}-{second},job-{host}-{third}")
+        dw.add_dependencies(f"job-{host}-{third},end")
+        dw.add_dependencies(f"start,job-{host}-{first}")
+
+    print(len(dw.jobs) == n)
+    g = str(dw.graph)
+    print(g)
+    assert "name: start" in g
+    assert "start-job-rivanna.hpc.virginia.edu-3:" in g
+    return dw
+
 
 
 class TestWorkflow:
@@ -171,8 +243,42 @@ class TestWorkflow:
         banner("Workflow")
         print(w.graph)
 
+    def test_remove_job(self):
+        HEADING()
+        create_dummy_workflow()
+        global dw
+        Benchmark.Start()
+        dw.remove_job('start')
+        dw.remove('end')
+        Benchmark.Stop()
+        g = str(dw.graph)
+        assert 'start' not in g
+        assert 'end' not in g
+        assert 'job-local-0' in g
+        assert 'job-rivanna-3' in g
+        if os_is_windows():
+            user = os.environ["USERNAME"]
+        else:
+            try:
+                user = os.environ['USER']
+            except:
+                # docker image does not have user variable. so just do basename of home
+                user = os.system('basename $HOME')
+        dw.add_job(name="start", kind='local', user=user, host=host)
+        dw.add_job(name="end", kind='local', user=user, host=host)
 
-class todo:
+
+    def test_remove_workflow(self):
+        HEADING()
+        create_dummy_workflow()
+        global dw
+        Benchmark.Start()
+        dw.remove_workflow()
+        Benchmark.Stop()
+        assert dw.graph is None
+        assert dw.nodes is None
+        assert dw.edges is None
+        create_dummy_workflow()
 
     def test_benchmark(self):
         HEADING()
