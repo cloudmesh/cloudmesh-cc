4a104e70fc76f264565c4b0cbece439a626dce14
Gregor von Laszewski <laszewski@gmail.com>
2022-07-21 07:47:22 -0400

    add a-b test

--- tests/test_workflow-a-b.py
+++ tests/test_workflow-a-b.py
@@ -33,123 +33,36 @@ variables = Variables()
 
 name = "run"
 
-if "host" not in variables:
-    host = "rivanna.hpc.virginia.edu"
-else:
-    host = variables["host"]
-
-username = variables["username"]
-
-if username is None:
-    Console.error("No username provided. Use cms set username=ComputingID")
-    quit()
 
 w = None
 
-try:
-    if not Vpn.enabled():
-        raise Exception('vpn not enabled')
-    check_output(f"ssh {username}@{host} hostname", stderr=STDOUT, timeout=6)
-    login_success = True
-except:  # noqa: E722
-    login_success = False
 
-
-class TestWorkflow:
+class TestWorkflowAB:
 
     def test_load_workflow(self):
         HEADING()
         global w
         Benchmark.Start()
         w = Workflow()
-        w.load("tests/workflow.yaml")
+        w.load("tests/workflow-a-b.yaml")
         Benchmark.Stop()
         print(w.graph)
 
-    def test_set_up(self):
-        """
-        establishing a queues object, saving 2 queues to it, each with 10 jobs
-        :return: no return
-        """
-        HEADING()
-        global w
-        global username
-        Benchmark.Start()
-        w = Workflow()
 
-        if os_is_windows():
-            localuser = os.environ["USERNAME"]
-        else:
-            localuser = os.environ['USER']
-        login = {
-            "localhost": {"user": f"{localuser}", "host": "local"},
-            "rivanna": {"user": f"{username}", "host": "rivanna.hpc.virginia.edu"},
-            "pi": {"user": f"{localuser}", "host": "red"},
-        }
-
-        n = 0
-
-        user = login["localhost"]["user"]
-        host = login["localhost"]["host"]
-
-        if os_is_windows():
-            jobkind = "wsl"
-        else:
-            jobkind = "local"
-
-        for script in ["start", "end"]:
-            Shell.copy(f"./tests/workflow-sh/{script}.sh",".")
-            assert os.path.isfile(f"./{script}.sh")
-            w.add_job(name=script, kind=jobkind, user=user, host=host)
-
-
-        for host, kind in [("localhost", jobkind),
-                           # ("rivanna", "remote-slurm"),
-                           ("rivanna", "ssh")]:
-            print("HOST:", host)
-            user = login[host]["user"]
-            host = login[host]["host"]
-
-            print(n)
-            w.add_job(name=f"job-{host}-{n}", kind=kind, user=user, host=host)
-            Shell.copy(f"./tests/workflow-sh/job-{host}-{n}.sh",".")
-            # os.system(f"cp ./tests/workflow-sh/job-{host}-{n}.sh .")
-            n = n + 1
-            w.add_job(name=f"job-{host}-{n}", kind=kind, user=user, host=host)
-            Shell.copy(f"./tests/workflow-sh/job-{host}-{n}.sh",".")
-            n = n + 1
-            w.add_job(name=f"job-{host}-{n}", kind=kind, user=user, host=host)
-            Shell.copy(f"./tests/workflow-sh/job-{host}-{n}.sh",".")
-            n = n + 1
-
-            first = n - 3
-            second = n - 2
-            third = n - 1
-            w.add_dependencies(f"job-{host}-{first},job-{host}-{second}")
-            w.add_dependencies(f"job-{host}-{second},job-{host}-{third}")
-            w.add_dependencies(f"job-{host}-{third},end")
-            w.add_dependencies(f"start,job-{host}-{first}")
-
-
-        Benchmark.Stop()
-        print(len(w.jobs) == n)
 
     def test_show(self):
         HEADING()
         global w
-        if os_is_windows():
-            w.graph.save(filename="test-dot.svg", colors="status", layout=nx.circular_layout, engine="dot")
-        else:
-            w.graph.save(filename="/tmp/test-dot.svg", colors="status", layout=nx.circular_layout, engine="dot")
-        # Shell.browser("/tmp/test-dot.svg")
-        # assert os.path.exists("~/tmp/test-dot.svg") == True
+        w.graph.save(filename="workflow-a-b.svg",
+                     colors="status",
+                     layout=nx.circular_layout, engine="dot")
 
     def test_get_node(self):
         HEADING()
         global w
         Benchmark.Start()
-        s1 = w["start"]
-        s2 = w.job("start")
+        s1 = w["a"]
+        s2 = w.job("a")
         Benchmark.Stop()
         print(s1)
         assert s1 == s2
@@ -158,9 +71,13 @@ class TestWorkflow:
         HEADING()
         global w
         Benchmark.Start()
-        print(w.table)
+        table = str(w.table)
+        print(table)
         Benchmark.Stop()
-        assert True
+        assert "['a']" in table
+        assert "test-a.sh" in table
+        assert "[]" in table
+        assert "test-b.sh" in table
 
     def test_order(self):
         HEADING()
@@ -170,15 +87,6 @@ class TestWorkflow:
         Benchmark.Stop()
         print(order)
         assert len(order) == len(w.jobs)
-
-    @pytest.mark.skipif(not login_success, reason=f"host {username}@{host} not found or VPN not enabled")
-    def test_run(self):
-        HEADING()
-        Benchmark.Start()
-        w.run_topo()
-        Benchmark.Stop()
-        banner("Workflow")
-        print(w.graph)
 
     def test_benchmark(self):
         HEADING()
