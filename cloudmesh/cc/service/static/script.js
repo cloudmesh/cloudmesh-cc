1456f0259471841392a5aa9a8738ffd21ad5f3cc
J.P <stapmoshun@gmail.com>
2022-09-19 20:11:12 -0400

    add column toggle

--- cloudmesh/cc/service/static/script.js
+++ cloudmesh/cc/service/static/script.js
@@ -1,1 +1,20 @@
-$('#example').DataTable();
+$(document).ready(function () {
+    var table = $('#example').DataTable({
+        "columnDefs": [
+            { "width": "5%", "targets": 0 }
+        ],
+        lengthMenu: [
+            [-1, 10, 25, 50],
+            ['All', 10, 25, 50],
+        ],
+    });
+        $('a.toggle-vis').on('click', function (e) {
+        e.preventDefault();
+
+        // Get the column API object
+        var column = table.column($(this).attr('data-column'));
+
+        // Toggle the visibility
+        column.visible(!column.visible());
+    });
+});
