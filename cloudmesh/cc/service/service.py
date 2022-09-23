20592564891fa0b16b7a1dce54cb1fde9980b7f5
J.P <70083705+stapmoshun@users.noreply.github.com>
2022-09-23 08:11:17 -0400

    add edit button to graph interface

--- cloudmesh/cc/service/service.py
+++ cloudmesh/cc/service/service.py
@@ -352,35 +352,6 @@ async def datatable_server(request, name_of_workflow: str):
                     </tr>
                 </tfoot>
             </table>
-            <script type="text/javascript">
-            $(document).ready(function () {
-                var table = $('#example').DataTable({
-                    "columnDefs": [
-                        { "width": "5%", "targets": 0 },
-                        { "targets": 0, visible: id_hidden},
-                        { "targets": 1, visible: name_hidden},
-                        { "targets": 2, visible: status_hidden},
-                        { "targets": 3, visible: host_hidden},
-                        { "targets": 4, visible: progress_hidden},
-                        { "targets": 5, visible: script_hidden}
-                    ],
-                    lengthMenu: [
-                        [-1, 10, 25, 50],
-                        ['All', 10, 25, 50],
-                    ],
-            });
-            $('a.toggle-vis').on('click', function (e) {
-                e.preventDefault();
-
-                // Get the column API object
-                var column = table.column($(this).attr('data-column'));
-
-                // Toggle the visibility
-                column.visible(!column.visible());
-            });
-
-            });
-            </script>
         """
         return table_html_template
     placeholder_dict = None
