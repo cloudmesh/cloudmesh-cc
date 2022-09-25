86e178d3401b3e5d1e432880bd75879725a4c029
J.P <70083705+stapmoshun@users.noreply.github.com>
2022-09-25 01:44:29 -0400

    remove redundant pulls

--- tests/mnist/mlp_mnist.sh
+++ tests/mnist/mlp_mnist.sh
@@ -10,13 +10,13 @@
 echo "# cloudmesh status=running progress=1 pid=$$"
 
 nvidia-smi --list-gpus
-python ~/cloudmesh-cc/tests/mnist/create_python.py
+cd ~
+python ~/cm/cloudmesh-cc/tests/mnist/create_python.py
 # conda install pip
 echo "# cloudmesh status=running progress=50 pid=$$"
 cd ~/reu2022/code/deeplearning/
-git pull
 pip install -r requirements.txt
-cd ~/cloudmesh-cc
+cd ~/cm/cloudmesh-cc
 pip install -e .
 #conda install pytorch torchvision -c pytorch
 #conda install py-cpuinfo
@@ -27,7 +27,6 @@ module load cudatoolkit/11.0.3-py3.8
 module load cuda/11.4.2
 module load cudnn/8.2.4.15
 module load anaconda/2020.11-py3.8
-mkdir ~/cm
 cd ~/cm
 pip install cloudmesh-installer -U
 cloudmesh-installer get cc
--- tests/mnist/prepare.sh
+++ tests/mnist/prepare.sh
@@ -2,12 +2,16 @@
 echo "# cloudmesh status=running progress=1 pid=$$"
 cd ~
 rm -rf ~/experiment/mlp_mnist
+rm -rf ~/experiment/run_all_rivanna
+cd ~
 git clone https://github.com/cybertraining-dsc/reu2022.git
 cd ~/reu2022
 git pull
 echo "# cloudmesh status=running progress=50 pid=$$"
 cd ~
+mkdir ~/cm
+cd ~/cm
 git clone https://github.com/cloudmesh/cloudmesh-cc.git
-cd ~/cloudmesh-cc
+cd ~/cm/cloudmesh-cc
 git pull
 echo "# cloudmesh status=running progress=100 pid=$$"
